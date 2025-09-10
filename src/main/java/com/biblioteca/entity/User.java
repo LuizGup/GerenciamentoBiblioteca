cbdd057112beab64933b80a5ca0ee6dad416c726
Luiz Carlos Filho <lcarlos.mln@gmail.com>
2025-09-10 00:49:54 -0300

    Update User entity and add UserStatus enum
    Corrected field names and annotations in User entity, added new fields for email, CPF, and registration date. Introduced UserStatus enum to represent user status.

--- src/main/java/com/biblioteca/entity/User.java
+++ src/main/java/com/biblioteca/entity/User.java
@@ -3,6 +3,8 @@ package com.biblioteca.entity;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 @Entity
 public class User {
@@ -10,9 +12,16 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Collum(nullable = false)
-    private String nome;
+    @Column(nullable = false)
+    private String name;
 
+    @Column(nullable = false, unique = true)
+    private String emai;
 
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    @Column(nullable = false)
+    private LocalDateTime registerDate;
 
 }
