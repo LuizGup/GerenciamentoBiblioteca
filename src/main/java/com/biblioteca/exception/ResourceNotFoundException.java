5dcdf616189be0f650de1245d6d1418e4c1188c5
Luiz Carlos Filho <lcarlos.mln@gmail.com>
2025-09-23 17:41:11 -0300

    Update ResourceNotFoundException.java

--- src/main/java/com/biblioteca/exception/ResourceNotFoundException.java
+++ src/main/java/com/biblioteca/exception/ResourceNotFoundException.java
@@ -3,10 +3,6 @@ package com.biblioteca.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-/**
- * Exceção lançada quando um recurso solicitado não é encontrado no sistema.
- * Retorna o status HTTP 404 (Not Found).
- */
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
 
